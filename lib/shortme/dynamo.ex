29b39e1874c3b8a8ae123f969d08288e0283ae41
Dan Norris <protochron@gmail.com>
2015-09-16 23:32:46 -0400

    Add some function docs

--- lib/shortme/dynamo.ex
+++ lib/shortme/dynamo.ex
@@ -4,23 +4,28 @@ defmodule Shortme.Dynamo do
   @default_length 6
   @salt "8229953145"
 
+  require Hashids
   # The Erlcloud AWS library is awful and we need to grab the Erlang record like so
   require Record
   Record.defrecord :aws_config, Record.extract(:aws_config, from_lib: "erlcloud/include/erlcloud_aws.hrl")
 
-  require Hashids
+  @doc """
+  Insert a URL into the database and calculate the short URL id
+  """
+  @spec insert(bitstring) :: tuple
   def insert(url) do
     hashlib = Hashids.new(salt: @salt)
 
-    case :erlcloud_ddb2.update_item(counter_name [{"Id", {:n, 0}}], "set UrlCount = UrlCount + :num", [{:expression_attribute_values, [{":num", 1}]}, {:return_values, :all_old}], Shortme.Dynamo.get_aws_config()) do
+    case :erlcloud_ddb2.update_item(counter_name() [{"Id", {:n, 0}}], "set UrlCount = UrlCount + :num", [{:expression_attribute_values, [{":num", 1}]}, {:return_values, :all_old}], Shortme.Dynamo.get_aws_config()) do
       {:ok, x} ->
         id = Enum.into(x, %{})["UrlCount"]
       {:error, {"ValidationException", _}} ->
-          :erlcloud_ddb2.put_item(counter_name, [{"Id", {:n, 0}}, {"UrlCount", {:n, 0}}], [], get_aws_config)
+          :erlcloud_ddb2.put_item(counter_name(), [{"Id", {:n, 0}}, {"UrlCount", {:n, 0}}], [], get_aws_config)
           id = 0
     end
 
     # Get the short URL we should use here
+    # The URL will grow as the id value increases
     key = Hashids.encode(hashlib, id)
 
     case :erlcloud_ddb2.put_item(table_name(), [{"Id", {:s, key}}, {"Url", {:s, url}}], [],
@@ -32,6 +37,10 @@ defmodule Shortme.Dynamo do
       end
   end
 
+  @doc """
+  Retrieve the full URL from a shortened URL key
+  """
+  @spec retrieve(bitstring) :: tuple
   def retrieve(key) do
     case :erlcloud_ddb2.get_item(table_name(), {"Id", {:s, key}}, [],
       get_aws_config) do
